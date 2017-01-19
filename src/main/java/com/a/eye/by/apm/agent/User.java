2a2539020a49cf279f9cbfbb24f008ab7c42116d
baiyang <baiyang.java@gmail.com>
2017-01-19 13:45:03 +0800

    add userClassFileTransformer

---
+++ src/main/java/com/a/eye/by/apm/agent/User.java
@@ -0,0 +1,9 @@
+package com.a.eye.by.apm.agent;
+
+public class User {
+
+    public void sayHello() {
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/main/java/com/a/eye/by/apm/agent/UserClassFileTransformer.java
@@ -0,0 +1,35 @@
+package com.a.eye.by.apm.agent;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
+public class UserClassFileTransformer implements ClassFileTransformer {
+
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+            ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+
+        if ("".equals(className)) {
+            try {
+                CtClass ctClass = ClassPool.getDefault().get(className.replace('/', '.'));
+
+                CtMethod ctMethod = ctClass.getDeclaredMethod("sayHello");
+
+                ctMethod.insertBefore("System.out.println(\"before sayHello----\");");
+                ctMethod.insertAfter("System.out.println(\"after sayHello----\");");
+
+                return ctClass.toBytecode();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+}
